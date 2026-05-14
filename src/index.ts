7fa20235c3d919afa7ba34ba168dcced9842aa2a
DeveloperSky95 <haneulgim95@gmail.com>
2026-05-14 18:06:57 +0900

    프로젝트 준비

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import express from "express";
+import * as process from "node:process";
+import * as console from "node:console";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || "8080";
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`서버가 실행되었습니다 http://localhost:${PORT}`);
+})
