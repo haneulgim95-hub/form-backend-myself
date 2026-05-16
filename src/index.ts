b54875a07517ef1a4887807c80f6292beb14d2c8
haneul <haneulgim95@gamil.com>
2026-05-16 22:56:57 +0900

    cors 에러 메시지 허용

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import express from "express";
 import * as process from "node:process";
 import * as console from "node:console";
 import userRouter from "./routes/userRouter.ts";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || "8080";
+
+app.use(cors({origin: "http://localhost:5173", credentials: true}));
 
 app.use(express.json());
 
