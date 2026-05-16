38a1de40a7ea1611f304e841650a0c6fa61287f0
haneul <haneulgim95@gamil.com>
2026-05-16 21:50:08 +0900

    createUser 에러 핸들링

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -25,6 +25,24 @@ const createUser = async (req: Request, res: Response) => {
         // res.json() 이 이미 JSON.stringify() 를 대신 해주는 함수라는 점만 추가로 알면 된다.
         res.status(201).json(newUser);
     } catch (error) {
+        if (error instanceof Error) {
+            switch(error.message) {
+                case "ALREADY_EXISTS_USERNAME":
+                    res.status(409).json({ message: "이미 사용 중인 아이디입니다"});
+                    return;
+                case "ALREADY_EXISTS_EMAIL":
+                    res.status(409).json({ message: "이미 가입된 이메일입니다."});
+                    return;
+                case "ALREADY_EXISTS_NICKNAME":
+                    res.status(409).json({ message: "이미 사용 중인 닉네임입니다."});
+                    return;
+                default:
+                    res.status(500).json({ message: "유저 생성 중 오류가 발생했습니다."});
+                    return;
+            }
+        }
+
+        console.log(error);
         res.status(500).json({message: "유저 생성 중 오류가 발생했습니다."});
     }
 };
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,6 @@
 import { UserCreateInput } from "../generated/prisma/models/User.ts";
 import prisma from "../config/prisma.ts";
+import { Prisma } from "../generated/prisma/client.ts";
 
 const createUser = async (data: UserCreateInput) => {
     try {
@@ -7,8 +8,23 @@ const createUser = async (data: UserCreateInput) => {
             data,
         });
     } catch (error) {
-        console.error(error);
-        throw error;
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === "P2002") {
+                const errorMessage = error.message;
+                if (errorMessage.includes("username")) {
+                    throw new Error("ALREADY_EXISTS_USERNAME");
+                }
+                if (errorMessage.includes("email")) {
+                    throw new Error("ALREADY_EXISTS_EMAIL");
+                }
+                if (errorMessage.includes("nickname")) {
+                    throw new Error("ALREADY_EXISTS_NICKNAME");
+                }
+                throw new Error("UNKNOWN_ERROR");
+            }
+        }
+        throw new Error("UNKNOWN_ERROR");
+
     }
 };
 
