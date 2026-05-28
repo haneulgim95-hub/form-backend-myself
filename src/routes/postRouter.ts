e3bdd36bd3cf365a064311c32a2b09712f9ba8c7
dvpsky <haneulgim95@gmail.com>
2026-05-28 23:23:03 +0900

    오류 수정

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import postController from "../controllers/postController.ts";
 import { validate } from "../middlewares/validate.ts";
 import { createPostSchema } from "../schemas/post/createPostSchema.ts";
+import { authenticate } from "../middlewares/auth.ts";
 
 const router = Router();
 
 router.get("/list/:categoryId", postController.getPostsByCategory);
-router.get("/create", validate(createPostSchema), );
+router.post("/create", authenticate, validate(createPostSchema), postController.createPost);
 
 export default router;
